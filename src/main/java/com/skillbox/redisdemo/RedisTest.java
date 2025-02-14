1a5f3a68e7a802b158ca9f3c6bfe2e4821b272b1
daniil0shcharbakou <daniil.shcharbakou@student.ehu.lt>
2025-02-14 11:09:58 +0200

    Update RedisTest.java

--- src/main/java/com/skillbox/redisdemo/RedisTest.java
+++ src/main/java/com/skillbox/redisdemo/RedisTest.java
@@ -1,51 +1,40 @@
-package com.skillbox.redisdemo;
+import org.redisson.Redisson;
+import org.redisson.api.RList;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Random;
 
-import static java.lang.System.out;
-
-public class RedisTest {
-
-    // Запуск докер-контейнера:
-    // docker run --rm --name skill-redis -p 127.0.0.1:6379:6379/tcp -d redis
-
-    // Для теста будем считать неактивными пользователей, которые не заходили 2 секунды
-    private static final int DELETE_SECONDS_AGO = 2;
-
-    // Допустим пользователи делают 500 запросов к сайту в секунду
-    private static final int RPS = 500;
-
-    // И всего на сайт заходило 1000 различных пользователей
-    private static final int USERS = 1000;
-
-    // Также мы добавим задержку между посещениями
-    private static final int SLEEP = 1; // 1 миллисекунда
-
-    private static final SimpleDateFormat DF = new SimpleDateFormat("HH:mm:ss");
-
-    private static void log(int UsersOnline) {
-        String log = String.format("[%s] Пользователей онлайн: %d", DF.format(new Date()), UsersOnline);
-        out.println(log);
-    }
-
+public class DatingSiteQueue {
     public static void main(String[] args) throws InterruptedException {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        RedissonClient redisson = Redisson.create(config);
 
-        RedisStorage redis = new RedisStorage();
-        redis.init();
-        // Эмулируем 10 секунд работы сайта
-        for(int seconds=0; seconds <= 10; seconds++) {
-            // Выполним 500 запросов
-            for(int request = 0; request <= RPS; request++) {
-                int user_id = new Random().nextInt(USERS);
-                redis.logPageVisit(user_id);
-                Thread.sleep(SLEEP);
+        RList<Integer> queue = redisson.getList("USER_QUEUE");
+        
+        if (queue.isEmpty()) {
+            for (int i = 1; i <= 20; i++) {
+                queue.add(i);
             }
-            redis.deleteOldEntries(DELETE_SECONDS_AGO);
-            int usersOnline = redis.calculateUsersNumber();
-            log(usersOnline);
         }
-        redis.shutdown();
+
+        Random random = new Random();
+
+        while (true) {
+            Integer currentUser = queue.remove(0);
+            System.out.println("— На главной странице показываем пользователя " + currentUser);
+
+            if (random.nextInt(10) == 0) {
+                int index = random.nextInt(queue.size());
+                Integer payingUser = queue.remove(index);
+                queue.add(0, payingUser);
+                System.out.println("> Пользователь " + payingUser + " оплатил платную услугу");
+            }
+
+            queue.add(currentUser);
+
+            Thread.sleep(1000);
+        }
     }
 }
